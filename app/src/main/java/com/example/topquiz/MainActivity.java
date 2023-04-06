1bb73b180bca04333d92c0450593bab9061e342a
Loic Callaerts <loic.callaerts@gmail.com>
2023-04-06 12:06:48 +0200

    Initial commit

---
+++ app/src/main/java/com/example/topquiz/MainActivity.java
@@ -0,0 +1,68 @@
+package com.example.topquiz;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Button;
+
+import com.example.topquiz.model.User;
+import com.example.topquizz.R;
+
+public class MainActivity extends AppCompatActivity {
+    private TextView mGreetingTextView;
+    private EditText mNameEditText;
+    private Button mPlayButton;
+    private static final int GAME_ACTIVITY_REQUEST_CODE = 42;
+    private static final String SHARED_PREF_USER_INFO = "SHARED_PREF_USER_INFO";
+    private static final int REQUEST_CODE_GAME_ACTIVITY = 42;
+    private static final String SHARED_PREF_USER_INFO_NAME = "SHARED_PREF_USER_INFO_NAME";
+    private static final String SHARED_PREF_USER_INFO_SCORE = "SHARED_PREF_USER_INFO_SCORE";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mGreetingTextView = findViewById(R.id.main_textview_greeting);
+        mNameEditText = findViewById(R.id.main_edittext_name);
+        mPlayButton = findViewById(R.id.main_button_play);
+        mPlayButton.setEnabled(false); //Désactive le bouton
+        User mUser = new User();
+        mNameEditText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                // This is where we'll check the user input
+                mPlayButton.setEnabled(!s.toString().isEmpty());
+            }
+        });
+
+        mPlayButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // The user just clicked
+                getSharedPreferences(SHARED_PREF_USER_INFO, MODE_PRIVATE)
+                        .edit()
+                        .putString(SHARED_PREF_USER_INFO_NAME, mNameEditText.getText().toString())
+                        .apply();
+
+                startActivityForResult(new Intent(MainActivity.this, GameActivity.class), REQUEST_CODE_GAME_ACTIVITY);
+            }
+        });
+    }
+}
+
+
